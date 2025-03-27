5945381d8caaefa35903da73286c02d4cc0bb949
wsadzanie2 <132954213+wsadzanie2@users.noreply.github.com>
2025-03-27 13:22:36 +0100

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+#Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -47,7 +47,11 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    # theme = "catpuccin-mocha";
+    # package = pkgs.kdePackages.sddm;
+  };
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
